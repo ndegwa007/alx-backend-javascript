bde01addefd13289cd3423a26c2fa9203c2c9aca
ndegz <ndegwa8781@gmail.com>
2023-07-31 19:08:00 +0300

    rooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,8 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const cls1 = new ClassRoom(19);
-  const cls2 = new ClassRoom(20);
-  const cls3 = new ClassRoom(34);
+  const roomSize = [19, 20, 34];
+  const room = roomSize.map((size) => new ClassRoom(size));
 
-  return `[
-    ClassRoom { _maxStudentsSize: ${cls1._maxStudentsSize} },
-    ClassRoom { _maxStudentsSize: ${cls2._maxStudentsSize} },
-    ClassRoom { _maxStudentsSize: ${cls3._maxStudentsSize} }
-]`;
+  return room;
 }
