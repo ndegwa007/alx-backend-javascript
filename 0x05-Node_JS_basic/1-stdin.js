17cd4d52b3df37900be03d72c5ea4101d90a691a
ndegz <ndegwa8781@gmail.com>
2023-10-04 13:15:25 +0300

    new fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,33 +1,10 @@
-#!/usr/bin/node
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-if (process.stdin.isTTY) {
-  // Running interactively with 'node [filename]'
-  console.log('Welcome to Holberton School, what is your name?');
-
-  rl.on('line', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
-  });
-} else {
-  // Piped input
-  console.log('Welcome to Holberton School, what is your name?');
-
-  const chunks = [];
-
-  process.stdin.on('data', (chunk) => {
-    chunks.push(chunk);
-  });
-
-  process.stdin.on('end', () => {
-    const input = chunks.join('');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
